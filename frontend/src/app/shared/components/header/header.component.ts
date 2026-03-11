d8a90a9a3a4287605fa79deb15c3439e0f6a1c2e
Prabhav Manas <prabhavmanas17@gmail.com>
2026-03-11 18:18:29 +0530

    reusable cards, header, TimeZen-logo, header-in-mobile-view, user-avatar-dropdown

---
+++ frontend/src/app/shared/components/header/header.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent implements OnInit{
+  isSidebarOpen = false;
+
+  constructor(){}
+
+  ngOnInit(): void {
+    
+  }
+
+  toggleSidebar(){
+    this.isSidebarOpen = !this.isSidebarOpen;
+  }
+
+  onCloseSideBar(){
+    this.isSidebarOpen=false;
+  }
+
+  logout(){}
+}
--- frontend/src/app/shared/shared.module.ts
+++ frontend/src/app/shared/shared.module.ts
@@ -1,18 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ButtonComponent } from './components/button/button.component';
+import { HeaderComponent } from './components/header/header.component';
+import { CardComponent } from './components/card/card.component';
 
 
 
 @NgModule({
   declarations: [
-    ButtonComponent
+    ButtonComponent,
+    HeaderComponent,
+    CardComponent
   ],
   imports: [
     CommonModule
   ],
   exports:[
-    ButtonComponent
+    ButtonComponent,
+    HeaderComponent,
+    CardComponent
   ]
 })
 export class SharedModule { }
