7ac2b18e76efa8ea2381ed9b89f7d21e7f10ace1
Prabhav Manas <prabhavmanas17@gmail.com>
2026-03-24 21:16:01 +0530

    check2

--- backend/src/utils/mailer.js
+++ backend/src/utils/mailer.js
@@ -21,26 +21,46 @@
 // }
 
 
-const sgMail = require('@sendgrid/mail');
+// const sgMail = require('@sendgrid/mail');
 
-sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+// sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+// exports.sendEmail = async (to, subject, html) => {
+//   try {
+//     const msg = {
+//       to,
+//       from: process.env.EMAIL_FROM, // must be verified in SendGrid
+//       subject,
+//       html,
+//     };
+
+//     const info = await sgMail.send(msg);
+//     console.log('Email sent successfully');
+//     return info;
+//   } catch (error) {
+//     console.error('Send email failed - Message:', error.message);
+//     console.error('Send email failed - Code:', error.code);
+//     console.error('Send email failed - Response:', JSON.stringify(error.response?.body));
+//     throw new Error('Failed to send email');
+//   }
+// };
+
+// mailer.js
+const { Resend } = require('resend');
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 exports.sendEmail = async (to, subject, html) => {
   try {
-    const msg = {
+    const data = await resend.emails.send({
+      from: 'TaskZen <onboarding@resend.dev>', // free tier default sender
       to,
-      from: process.env.EMAIL_FROM, // must be verified in SendGrid
       subject,
       html,
-    };
-
-    const info = await sgMail.send(msg);
-    console.log('Email sent successfully');
-    return info;
+    });
+    console.log('Email sent:', data.id);
+    return data;
   } catch (error) {
-    console.error('Send email failed - Message:', error.message);
-    console.error('Send email failed - Code:', error.code);
-    console.error('Send email failed - Response:', JSON.stringify(error.response?.body));
+    console.error('Send email failed:', error.message);
     throw new Error('Failed to send email');
   }
 };
