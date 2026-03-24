ce947a785ba33d46472c7f8c2330b963e5a1c5bb
Prabhav Manas <prabhavmanas17@gmail.com>
2026-03-24 14:28:28 +0530

    modified

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment={
     production:true,
-    apiUrl:'https://api.taskzen.com/api',
+    apiUrl:'https://taskzen-backend-b9cy.onrender.com',
     app:{
         name:'TaskZen',
         appVersion:'1.0.0',
