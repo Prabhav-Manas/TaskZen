19b0a1bb01825f5fa9817b25ae865290cf8de5ae
Prabhav Manas <prabhavmanas17@gmail.com>
2026-06-21 21:54:18 +0530

    Update production backend URL

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,6 +1,17 @@
+// export const environment={
+//     production:true,
+//     apiUrl:'https://taskzen-backend-b9cy.onrender.com/api',
+//     app:{
+//         name:'TaskZen',
+//         appVersion:'1.0.0',
+//         defaultLanguage:'en',
+//     }
+// }
+
+
 export const environment={
     production:true,
-    apiUrl:'https://taskzen-backend-b9cy.onrender.com/api',
+    apiUrl:'https://taskzen-backend-hlbe.onrender.com/api',
     app:{
         name:'TaskZen',
         appVersion:'1.0.0',
